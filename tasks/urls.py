a138b4df769c99968d90ec37064a90f8b4f78087
Adrian <adrian.adam.zaleski@gmail.com>
2021-12-05 13:54:36 +0100

    adds create task form - basic layout

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import TaskList, DetailView
+from .views import TaskList, DetailView, TaskCreate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', DetailView.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
 
     ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,5 @@
-from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
-
+from django.views.generic import CreateView, ListView, DetailView
+from django.urls import reverse_lazy
 from .models import Task
 
 # Create your views here.
@@ -15,3 +14,9 @@ class DetailView(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'tasks/task.html'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
