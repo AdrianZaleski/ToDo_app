74cbd1aa4d9cbbbd03866576a1546343c809e851
Adrian <adrian.adam.zaleski@gmail.com>
2021-12-08 17:17:25 +0100

    added Update view and Delete Task function

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -19,4 +19,16 @@ class DetailView(DetailView):
 class TaskCreate(CreateView):
     model = Task
     fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
     success_url = reverse_lazy('tasks')
